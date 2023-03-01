965dd514264edb68aae1fb9e8cb66fdde6469c57
idris <alparslan7887@gmail.com>
2023-03-01 21:20:08 +0300

    view eklendi

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const express = require("express");
 //express modülü bir nesne return eden bir fonksiyon içerir ve bu fonksiyonu biz express değişkenine atarız.
+const express = require("express");
 
 
 
-const app = express();
 // Bu kod bize object döndürür.
+const app = express();
 
+//Template Engine
+app.set("view engine","ejs")
+
+//MIDDLEWARES
+
+//app.use() Herhangi bir HTTP isteği için çalıştırılacak fonksiyonları belirler
+app.use(express.static("public"))
+
+
+//ROUTES
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+  res.status(200).render('index',{
+    page_name: 'index'
+  });
 });
+
+app.get("/about", (req, res) => {
+  res.status(200).render('about',{
+    page_name: 'about'
+  });
+});
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
